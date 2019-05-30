3a697f3bcd54af906c523bca72998900a0d504c0
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2019-05-30 17:07:08 +0200

    add anitmation

--- src/components/Song.svelte
+++ src/components/Song.svelte
@@ -2,6 +2,8 @@
   export let song;
   export let expand = false;
 
+  import { slide } from "svelte/transition";
+
   $: lyric_list = song.lyrics.split("\n");
 </script>
 
@@ -14,10 +16,13 @@
 <div class="container">
   <h2 on:click={() => (expand = !expand)}>{song.title}</h2>
   {#if expand}
-    {#each lyric_list as row}
-      <div>
-        {@html row}
-      </div>
-    {/each}
+    <div transition:slide>
+      {#each lyric_list as row, i}
+        <!--<div transition:fly={{y: -10*i, duration: 350}}>-->
+        <div>
+          {@html row}
+        </div>
+      {/each}
+    </div>
   {/if}
 </div>
