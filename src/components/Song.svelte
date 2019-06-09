ed6176507660e2b10fe56e9197c0d9e985fad1ab
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2019-06-09 18:41:18 +0200

    such_style

--- src/components/Song.svelte
+++ src/components/Song.svelte
@@ -1,22 +1,54 @@
 <script>
   export let song;
   export let expand = false;
+  export let selecter;
 
   import { slide } from "svelte/transition";
 
   $: lyric_list = song.lyrics.split("\n");
+  // TODO scroll into view
+  // formatta
 </script>
 
 <style>
-  .container {
-    margin-bottom: 0.7em;
+  .enlarge {
+  }
+
+  .song {
+    padding-top: 1em;
+    padding-bottom: 1em;
+  }
+
+  div {
+    font-size: 1.2em;
+  }
+
+  h1 {
+    margin: 0;
+  }
+
+  h3 {
+    margin: 0;
+    font-style: italic;
+    color: rgb(104, 104, 104);
+  }
+
+  button {
+    color: inherit;
+    border: none;
+    display: block;
+    background-color: #fccfff;
+    width: 100%;
+    padding: 0.3em;
+    margin-bottom: 0.3em;
   }
 </style>
 
-<div class="container">
-  <h2 on:click={() => (expand = !expand)}>{song.title}</h2>
+<button class:enlarge={expand} on:click={() => selecter(song.title)}>
+  <h1>{song.title}</h1>
+  <h3>Mel: {song.melodyTitle}</h3>
   {#if expand}
-    <div transition:slide>
+    <div class="song" transition:slide>
       {#each lyric_list as row, i}
         <div>
           {@html row}
@@ -24,4 +56,5 @@
       {/each}
     </div>
   {/if}
-</div>
+  <span class="song" />
+</button>
--- src/components/SongList.svelte
+++ src/components/SongList.svelte
@@ -1,9 +1,14 @@
 <script>
+  import Song from "./Song.svelte";
+
   export let songs;
   export let empty_list_text = "Inga sånger matchade din sökning";
 
-  import Song from "./Song.svelte";
   $: only_one_song = songs.length === 1;
+
+  let selected_title = "";
+  const selecter = title =>
+    selected_title === title ? (selected_title = "") : (selected_title = title);
 </script>
 
 <style>
@@ -15,7 +20,10 @@
 
 <div>
   {#each songs as song}
-    <Song {song} expand={only_one_song} />
+    <Song
+      {song}
+      expand={only_one_song || selected_title === song.title}
+      {selecter} />
   {:else}
     <h2>{empty_list_text}</h2>
   {/each}
